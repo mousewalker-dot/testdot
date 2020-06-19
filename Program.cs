ea92f82f0151cdbc996c7006fae6ad73fa8e9c9b
mousewalker-dot <67165687+mousewalker-dot@users.noreply.github.com>
2020-06-19 20:25:02 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-Console.WriteLine("Hello World!");
-Console.WriteLine("The current time is " + DateTime.Now);
+using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
